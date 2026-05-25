99cdd71fb0e26042d02e43e60011bf183d7a2e59
akramescheba <meschebajordy@gmail.com>
2026-05-25 13:29:52 +0200

    Ajout de la form principale de la page de démarrage

--- app.ps1
+++ app.ps1
@@ -1,1 +1,10 @@
+Import-Module .\logiques.ps1
 
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Page de démarrage"
+$form.Size = New-Object System.Drawing.Size(500, 550)
+
+$form.ShowDialog()
