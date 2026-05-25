909ace2957d393ec55a00ce76f1a2e7c7c544465
akramescheba <meschebajordy@gmail.com>
2026-05-25 20:55:20 +0200

    Ajout de logBox, de la fonctionnalité de copie et de warning en cas de chemis source/destination non défini

--- app.ps1
+++ app.ps1
@@ -8,6 +8,80 @@ $form.Text = "Page de démarrage"
 $form.Size = New-Object System.Drawing.Size(500, 550)
 
 # =============================================================
+# HeaderLabel
+# =============================================================
+$HeaderLabel = New-Object System.Windows.Forms.Label
+$HeaderLabel.Size = New-Object System.Drawing.Size(470, 55)
+$HeaderLabel.Location = New-Object System.Drawing.Point(5, 30)
+# $HeaderLabel.BackColor = [System.Drawing.Color]::Red
+$HeaderLabel.ForeColor = [System.Drawing.Color]::Black
+
+$form.Controls.Add($HeaderLabel)
+# =============================================================
+# TextBoxSource
+# =============================================================
+$TextBoxSource = New-Object System.Windows.Forms.TextBox
+$TextBoxSource.Text = ""
+$TextBoxSource.Size = New-Object System.Drawing.Size(400, 30)
+$TextBoxSource.Location = New-Object System.Drawing.Point(0, 3)
+$TextBoxSource.BackColor = [System.Drawing.Color]::FromArgb(214,214,214)
+$TextBoxSource.ForeColor = [System.Drawing.Color]::Black
+
+$HeaderLabel.Controls.Add($TextBoxSource)
+
+# =============================================================
+# Button Source
+# =============================================================
+$btnSource = New-Object System.Windows.Forms.Button
+$btnSource.Text = "..."
+$btnSource.Size = New-Object System.Drawing.Size(40, 20)
+$btnSource.Location = New-Object System.Drawing.Point(410, 3)
+$btnSource.BackColor = [System.Drawing.Color]::FromArgb(214,214,214)
+$btnSource.ForeColor = [System.Drawing.Color]::Black
+
+$btnSource.Add_Click({FolderBrowserDialogSouce})
+
+$HeaderLabel.Controls.Add($btnSource)
+
+# =============================================================
+# TextBoxDestination
+# =============================================================
+$TextBoxDestination = New-Object System.Windows.Forms.TextBox
+$TextBoxDestination.Text = ""
+$TextBoxDestination.Size = New-Object System.Drawing.Size(400, 30)
+$TextBoxDestination.Location = New-Object System.Drawing.Point(0, 30)
+$TextBoxDestination.BackColor = [System.Drawing.Color]::FromArgb(214,214,214)
+$TextBoxDestination.ForeColor = [System.Drawing.Color]::Black
+
+$HeaderLabel.Controls.Add($TextBoxDestination)
+
+# =============================================================
+# Button destination
+# =============================================================
+$btnDestination = New-Object System.Windows.Forms.Button
+$btnDestination.Text = "..."
+$btnDestination.Size = New-Object System.Drawing.Size(40, 20)
+$btnDestination.Location = New-Object System.Drawing.Point(410, 30)
+$btnDestination.BackColor = [System.Drawing.Color]::FromArgb(214,214,214)
+$btnDestination.ForeColor = [System.Drawing.Color]::Black
+
+$btnDestination.Add_Click({FolderBrowserDialogDestination})
+
+$HeaderLabel.Controls.Add($btnDestination)
+
+# =============================================================
+# LogBox
+# =============================================================
+$logBox = New-Object System.Windows.Forms.RichTextBox
+$logBox.Text = ""
+$logBox.Size = New-Object System.Drawing.Size(475, 350)
+$logBox.Location = New-Object System.Drawing.Point(5, 100)
+$logBox.BackColor = [System.Drawing.Color]::FromArgb(10,12,26)
+$logBox.ForeColor = [System.Drawing.Color]::White
+
+$form.Controls.Add($logBox)
+
+# =============================================================
 # FooterLabel
 # =============================================================
 
@@ -20,11 +94,16 @@ $FooterLabel.Location = New-Object System.Drawing.Point(3, 460)
 # Button copy
 # =============================================================
 $btnCopy = New-Object System.Windows.Forms.Button
-$btnCopy.Text = "Démarrer la copié"
+$btnCopy.Text = "Démarrage de copie"
 $btnCopy.Size = New-Object System.Drawing.Size(150, 30)
 $btnCopy.Location = New-Object System.Drawing.Point(3, 5)
 $btnCopy.BackColor = [System.Drawing.Color]::Blue
 $btnCopy.ForeColor = [System.Drawing.Color]::White
+$btnCopy.Add_Click({
+    $source = $TextBoxSource.Text.Trim()  
+    $destination  = $TextBoxDestination.Text.Trim()
+    Copy-ToDestination -Path -Source $source -Destination $destination -Force
+})
 
 $FooterLabel.Controls.Add($btnCopy)
 
@@ -38,6 +117,7 @@ $btnSynchronisation.Location = New-Object System.Drawing.Point(160, 5)
 $btnSynchronisation.BackColor = [System.Drawing.Color]::Green
 $btnSynchronisation.ForeColor = [System.Drawing.Color]::White
 
+$btnSynchronisation.Add_Click({Write-Log })
 $FooterLabel.Controls.Add($btnSynchronisation)
 # =============================================================
 # Button rédémarrer
--- logiques.ps1
+++ logiques.ps1
@@ -6,3 +6,68 @@ function FnClose {
 function FnRedemarrer {
     powershell -ExecutionPolicy ByPass -File app.ps1
 }
+
+function FolderBrowserDialogSouce ($dialog) {
+    $dialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    if ($dialog.ShowDialog() -eq "OK") {
+        $TextBoxSource.Text = $dialog.SelectedPath 
+    }
+}
+
+function FolderBrowserDialogDestination ($dialog) {
+    $dialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    if ($dialog.ShowDialog() -eq "OK") {
+        $TextBoxDestination.Text = $dialog.SelectedPath 
+    }
+
+}
+function Write-Log {
+    param([string]$Message)
+    $texte = "[ $(Get-Date -Format "dd-MM-yyyy HH:mm:ss") ] - $Message`n"
+
+    $logBox.AppendText($texte)
+}
+
+function Copy-ToDestination {
+
+    param(
+        [string]$Source,
+        [string]$Destination
+    )
+
+    if(-not ($Source)) { 
+        $confirmation = [System.Windows.Forms.MessageBox]::Show(
+            "Source non définie",  "Confirmation", "OK", "Warning")
+        if ($confirmation -eq "OK") {
+            return
+        }
+
+    }
+      if(-not ($Destination)) { 
+        $confirmation =  [System.Windows.Forms.MessageBox]::Show(
+            "Destination non définie",  "Confirmation","OK", "Warning")
+        if ($confirmation -eq "OK") {
+            return
+        }
+
+    }
+
+    $fichiers = Get-ChildItem -Path $Source -File
+    $succes++
+    $echec++
+
+    Write-Log "INFO : < ======= Début de la copie ======= >`n"
+
+    foreach ($fichier in $fichiers) {
+         try{
+        Copy-Item -Path $fichier.FullName -Force
+          $succes++
+        Write-Log "COPIE : $($fichier.Name)"
+}catch{
+    $echec++
+}
+    }
+     Write-Log "INFO : $($fichiers.Count) copiés`n"
+     Write-Log "INFO : < ======= Fin de la copie ======= >`n"
+
+}
